dd1e3359fa3ad1710c2f3b5280c0ea64907bd42f
yuttasak <yuttasak@tconio.com>
2022-01-21 23:57:11 +0700

    fixed

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import win32api
 import win32con
 import logging
 import asyncio
+import requests
 
 
 class MyBot():
@@ -21,6 +22,7 @@ class MyBot():
         self.start_time = 0
         self.time_loop_swap = 0
         self.time_loop_refresh = 0
+        self.token_notify = self.configObject["LINE_NOTIFY"]["TOKEN"]
         self.isConnect = int(self.configObject["POSITIONS"]["IS_CONNECT"])
         self.posConnectWallet = (int(self.configObject["POSITIONS"]["CONNECT_WALLET_X"]), int(
             self.configObject["POSITIONS"]["CONNECT_WALLET_Y"]))
@@ -48,6 +50,15 @@ class MyBot():
             int(self.configObject["POSITIONS"]["DELAY_LOOP_REFRESH_STAMINA"])
         self.processing = False
         self.loop = asyncio.get_event_loop()
+
+    def capture_screen(self):
+        try:
+            self.box = (450, 378, 1086, 850)
+            img = self.grab.grab(self.box)
+            img.save("picture.jpg")
+            print("capture screen success")
+        except:
+            print('capture screen error')
 
     def mouseClick(self, cord):
         win32api.SetCursorPos((cord[0], cord[1]))
@@ -94,6 +105,7 @@ class MyBot():
         now = int(time.time() * 1000)
         if self.start_time > 0 and self.start_time > 0 and now - self.time_loop_swap > self.delayLoopSwapPage * 1000:
             logging.info("Loop Swap Page Start")
+            self.linenotify("Loop swap page working")
             self.time_loop_swap = int(time.time() * 1000)
             print('Loop Swap Page Start')
             self.mouseClick(self.posWallet)
@@ -105,6 +117,7 @@ class MyBot():
             self.mouseClick(self.posExitWallet)
             logging.info(
                 f"Loop Swap Page Stop Pending {self.delayLoopSwapPage} Second")
+            self.linenotify("Loop swap page Worked")
 
     def loopRefreshStamina(self):
         now = int(time.time() * 1000)
@@ -112,6 +125,7 @@ class MyBot():
             self.time_loop_refresh = int(time.time() * 1000)
             logging.info("Loop Refresh Stamina Start")
             print("Loop Refresh Stamina Start")
+            self.linenotify("Loop refresh stamina working")
             self.mouseClick(self.posPauseAndSelectHeroes)
             logging.info("Select Button Pause Game")
             time.sleep(3)
@@ -137,17 +151,36 @@ class MyBot():
             logging.info(
                 f"Loop Refresh Stamina Stop Pending {self.delayLoopRefreshStamina} Second")
             self.processing = False
+            self.linenotify("Loop refresh stamina worked")
+
+    def linenotify(self, message):
+        try:
+            url = 'https://notify-api.line.me/api/notify'
+            token = self.token_notify  # Line Notify Token
+            # Local picture File
+            img = {'imageFile': open('picture.jpg', 'rb')}
+            data = {'message': message}
+            headers = {'Authorization': 'Bearer ' + token}
+            session = requests.Session()
+            session_post = session.post(
+                url, headers=headers, files=img, data=data)
+            print("send line notify success")
+        except:
+            print("send line notify error")
 
     def run(self):
-        if self.isConnect == 1:
-            self.startGame()
-        else:
-            self.start_time = int(time.time() * 1000)
+        # if self.isConnect == 1:
+        #     self.startGame()
+        # else:
+        self.capture_screen()
+        self.start_time = int(time.time() * 1000)
         while True:
+            self.capture_screen()
             self.loopSwapPage()
             self.loopRefreshStamina()
             print('sleep 10 second')
             time.sleep(10)
+
 
 bot = MyBot()
 event_loop = asyncio.get_event_loop()
