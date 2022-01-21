cb3da28d7816c3fa8c4d8fcbe68a99a48291066e
James Ncm <jamesncm123@gmail.com>
2022-01-22 05:24:27 +0700

    fixed

--- app.py
+++ app.py
@@ -20,14 +20,7 @@ class MyBot():
         self.pathPhoto = os.path.join(self.mainPath, "photo")
         self.box = ()
         self.start_time = 0
-        self.time_loop_swap = 0
-        self.time_loop_refresh = 0
         self.token_notify = self.configObject["LINE_NOTIFY"]["TOKEN"]
-        # self.isConnect = int(self.configObject["POSITIONS"]["IS_CONNECT"])
-        # self.posConnectWallet = (int(self.configObject["POSITIONS"]["CONNECT_WALLET_X"]), int(
-        #     self.configObject["POSITIONS"]["CONNECT_WALLET_Y"]))
-        # self.posMetaSign = (int(self.configObject["POSITIONS"]["META_SIGN_X"]), int(
-        #     self.configObject["POSITIONS"]["META_SIGN_Y"]))
         self.posImageLeftTopX = int(
             self.configObject["IMAGE_POSITION"]["LEFT_TOP_X"])
         self.posImageLeftTopY = int(
@@ -36,27 +29,39 @@ class MyBot():
             self.configObject["IMAGE_POSITION"]["RIGHT_BUTTOM_X"])
         self.posImageRightButtomY = int(
             self.configObject["IMAGE_POSITION"]["RIGHT_BUTTOM_Y"])
-        self.posHeroes = (int(self.configObject["POSITIONS"]["HEROES_X"]), int(
-            self.configObject["POSITIONS"]["HEROES_Y"]))
-        self.posReset = (int(self.configObject["POSITIONS"]["RESET_X"]), int(
-            self.configObject["POSITIONS"]["RESET_Y"]))
-        self.posAll = (int(self.configObject["POSITIONS"]["ALL_X"]), int(
-            self.configObject["POSITIONS"]["ALL_Y"]))
-        self.posExitHeroes = (int(self.configObject["POSITIONS"]["EXIT_HEROES_X"]), int(
-            self.configObject["POSITIONS"]["EXIT_HEROES_Y"]))
-        self.posTreasureHunt = (int(self.configObject["POSITIONS"]["TREASUREHUNT_X"]), int(
-            self.configObject["POSITIONS"]["TREASUREHUNT_Y"]))
-        self.posWallet = (int(self.configObject["POSITIONS"]["WALLET_X"]), int(
-            self.configObject["POSITIONS"]["WALLET_Y"]))
-        self.posExitWallet = (int(self.configObject["POSITIONS"]["EXIT_WALLET_X"]), int(
-            self.configObject["POSITIONS"]["EXIT_WALLET_Y"]))
-        self.posPauseAndSelectHeroes = (int(self.configObject["POSITIONS"]["PAUSE_AND_SELECT_HEROES_X"]), int(
-            self.configObject["POSITIONS"]["PAUSE_AND_SELECT_HEROES_Y"]))
-        self.delayLoopSwapPage = 60 * \
-            int(self.configObject["POSITIONS"]["DELAY_LOOP_SWAP_PAGE"])
-        self.delayLoopRefreshStamina = 60 * \
-            int(self.configObject["POSITIONS"]["DELAY_LOOP_REFRESH_STAMINA"])
-        self.processing = False
+
+        self.screenUnit = int(
+            self.configObject["SCREEN"]["SCREEN_UNIT"])
+        self.positions = []
+        for i in range(self.screenUnit):
+            position = "POSITION_"+str(i+1)
+            if position in self.configObject:
+                bodyPosition = {
+                    "index": i,
+                    "posWallet": (int(self.configObject[position]["WALLET_X"]), int(
+                        self.configObject[position]["WALLET_Y"])),
+                    "posReset": (int(self.configObject[position]["RESET_X"]), int(
+                        self.configObject[position]["RESET_Y"])),
+                    "posAll": (int(self.configObject[position]["ALL_X"]), int(
+                        self.configObject[position]["ALL_Y"])),
+                    "posExitHeroes": (int(self.configObject[position]["EXIT_HEROES_X"]), int(
+                        self.configObject[position]["EXIT_HEROES_Y"])),
+                    "posTreasureHunt": (int(self.configObject[position]["TREASUREHUNT_X"]), int(
+                        self.configObject[position]["TREASUREHUNT_Y"])),
+                    "posExitWallet": (int(self.configObject[position]["EXIT_WALLET_X"]), int(
+                        self.configObject[position]["EXIT_WALLET_Y"])),
+                    "posPauseAndSelectHeroes": (int(self.configObject[position]["PAUSE_AND_SELECT_HEROES_X"]), int(
+                        self.configObject[position]["PAUSE_AND_SELECT_HEROES_Y"])),
+                    "delayLoopSwapPage": 60 *
+                    int(self.configObject[position]
+                        ["DELAY_LOOP_SWAP_PAGE"]),
+                    "delayLoopRefreshStamina": 60 *
+                    int(self.configObject[position]
+                        ["DELAY_LOOP_REFRESH_STAMINA"]),
+                    "timeLoopSwap": 0,
+                    "timeLoopRefresh": 0
+                }
+                self.positions.append(bodyPosition)
         self.loop = asyncio.get_event_loop()
 
     def capture_screen(self):
@@ -71,96 +76,70 @@ class MyBot():
 
     def mouseClick(self, cord):
         win32api.SetCursorPos((cord[0], cord[1]))
+        time.sleep(2)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-        time.sleep(0.3)
+        time.sleep(0.2)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
-    # def startGame(self):
-    #     logging.info('Start Game')
-    #     self.mouseClick(self.posConnectWallet)
-    #     logging.info('Connect Wallet')
-    #     time.sleep(5)
-    #     self.mouseClick(self.posMetaSign)
-    #     logging.info('Connect Meta Sign')
-    #     time.sleep(10)
-    #     self.mouseClick(self.posHeroes)
-    #     time.sleep(1)
-    #     self.mouseClick(self.posHeroes)
-    #     logging.info('Select Button Heroes')
-    #     time.sleep(3)
-    #     self.mouseClick(self.posReset)
-    #     time.sleep(1)
-    #     self.mouseClick(self.posReset)
-    #     logging.info('Select Button Reset')
-    #     time.sleep(3)
-    #     self.mouseClick(self.posAll)
-    #     time.sleep(1)
-    #     self.mouseClick(self.posAll)
-    #     logging.info('Select Button All')
-    #     time.sleep(3)
-    #     self.mouseClick(self.posExitHeroes)
-    #     time.sleep(1)
-    #     self.mouseClick(self.posExitHeroes)
-    #     logging.info('Select Button Exit')
-    #     time.sleep(3)
-    #     self.mouseClick(self.posTreasureHunt)
-    #     time.sleep(1)
-    #     self.mouseClick(self.posTreasureHunt)
-    #     logging.info('Go to Game')
-    #     time.sleep(1)
-    #     self.start_time = int(time.time() * 1000)
-
-    def loopSwapPage(self):
+    def loopSwapPage(self, position):
         now = int(time.time() * 1000)
-        if self.start_time > 0 and self.start_time > 0 and now - self.time_loop_swap > self.delayLoopSwapPage * 1000:
+        if self.start_time > 0 and self.start_time > 0 \
+                and now - position["timeLoopSwap"] > position["delayLoopSwapPage"] * 1000:
             logging.info("Loop Swap Page Start")
-            self.linenotify("Loop swap page working")
-            self.time_loop_swap = int(time.time() * 1000)
-            print('Loop Swap Page Start')
-            self.mouseClick(self.posWallet)
+            self.positions[position["index"]]["timeLoopSwap"] = int(
+                time.time() * 1000)
+            print("Screen "+str(position["index"]+1)+", Loop Swap Page Start")
+            self.mouseClick(position["posWallet"])
             time.sleep(1)
-            self.mouseClick(self.posWallet)
+            # self.mouseClick(position["posWallet"])
+            # time.sleep(3)
+            self.capture_screen()
+            time.sleep(1)
+            self.linenotify("Screen "+str(position["index"]+1) +
+                            ", Loop swap page Working")
             time.sleep(3)
-            self.mouseClick(self.posExitWallet)
+            self.mouseClick(position["posExitWallet"])
             time.sleep(1)
-            self.mouseClick(self.posExitWallet)
+            self.mouseClick(position["posExitWallet"])
             logging.info(
-                f"Loop Swap Page Stop Pending {self.delayLoopSwapPage} Second")
-            self.linenotify("Loop swap page Worked")
+                "Loop Swap Page Stop Pending"+str(position["delayLoopSwapPage"])+" Second")
 
-    def loopRefreshStamina(self):
+    def loopRefreshStamina(self, position):
         now = int(time.time() * 1000)
-        if self.start_time > 0 and now - self.time_loop_refresh > self.delayLoopRefreshStamina * 1000:
-            self.time_loop_refresh = int(time.time() * 1000)
+        if self.start_time > 0 and now - position["timeLoopRefresh"] > position["delayLoopRefreshStamina"] * 1000:
+            self.positions[position["index"]]["timeLoopRefresh"] = int(
+                time.time() * 1000)
             logging.info("Loop Refresh Stamina Start")
             print("Loop Refresh Stamina Start")
-            self.linenotify("Loop refresh stamina working")
-            self.mouseClick(self.posPauseAndSelectHeroes)
+            self.mouseClick(position["posPauseAndSelectHeroes"])
             logging.info("Select Button Pause Game")
-            time.sleep(3)
-            self.mouseClick(self.posPauseAndSelectHeroes)
+            time.sleep(1)
+            self.mouseClick(position["posPauseAndSelectHeroes"])
             logging.info("Select Button Heroes")
-            time.sleep(3)
-            self.mouseClick(self.posReset)
             time.sleep(1)
-            self.mouseClick(self.posReset)
+            self.mouseClick(position["posReset"])
+            time.sleep(1)
+            # self.mouseClick(position["posReset"])
             logging.info("Select Button Reset")
-            time.sleep(3)
-            self.mouseClick(self.posAll)
             time.sleep(1)
-            self.mouseClick(self.posAll)
+            self.mouseClick(position["posAll"])
+            time.sleep(1)
+            # self.mouseClick(position["posAll"])
+            # time.sleep(3)
+            self.capture_screen()
+            time.sleep(3)
+            self.linenotify("Screen "+str(position["index"]+1) +
+                            ", Loop refresh stamina working")
             logging.info("Select Button All")
             time.sleep(3)
-            self.mouseClick(self.posExitHeroes)
+            self.mouseClick(position["posExitHeroes"])
             time.sleep(1)
-            self.mouseClick(self.posExitHeroes)
+            # self.mouseClick(position["posExitHeroes"])
             logging.info("Go to Game")
             time.sleep(3)
-            self.mouseClick(self.posTreasureHunt)
+            self.mouseClick(position["posTreasureHunt"])
             logging.info(
-                f"Loop Refresh Stamina Stop Pending {self.delayLoopRefreshStamina} Second")
-            self.processing = False
-            self.linenotify("Loop refresh stamina worked")
+                "Loop Refresh Stamina Stop Pending " + str(position["delayLoopRefreshStamina"])+" Second")
 
     def linenotify(self, message):
         try:
@@ -174,6 +153,7 @@ class MyBot():
             session_post = session.post(
                 url, headers=headers, files=img, data=data)
             print("send line notify success")
+            print("session_post")
         except:
             print("send line notify error")
 
@@ -181,12 +161,13 @@ class MyBot():
         # if self.isConnect == 1:
         #     self.startGame()
         # else:
-        self.capture_screen()
         self.start_time = int(time.time() * 1000)
+        self.capture_screen()
         while True:
-            self.capture_screen()
-            self.loopSwapPage()
-            self.loopRefreshStamina()
+            if len(self.positions) > 0:
+                for position in self.positions:
+                    self.loopSwapPage(position)
+                    self.loopRefreshStamina(position)
             print('sleep 10 second')
             time.sleep(10)
 
